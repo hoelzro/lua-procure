ea222bf1affa52eae3a1b655a547d541a63d8629
Rob Hoelz <rob@hoelz.ro>
2012-08-26 22:27:30 +0200

    Add pure Lua versions of the loaders

--- require/init.lua
+++ require/init.lua
@@ -1,7 +1,80 @@
+local sformat      = string.format
+local sgmatch      = string.gmatch
+local sgsub        = string.gsub
+local smatch       = string.match
+local tconcat      = table.concat
+local tinsert      = table.insert
+local setmetatable = setmetatable
+local ploadlib     = package.loadlib
+
 local meta = {}
 local _M   = setmetatable({}, meta)
 
 _M.VERSION = '0.01'
+
+-- XXX assert(type(package.preload[name]) == 'function')?
+local function preload_loader(name)
+  if package.preload[name] then
+    return package.preload[name]
+  else
+    return sformat("no field package.preload['%q']\n", name)
+  end
+end
+
+local function path_loader(name, paths, loader_func)
+  local errors = {}
+  local loader
+
+  for path in sgmatch(paths, '[^;]+') do
+    path = sgsub(path, '%?', name)
+
+    local errmsg
+
+    loader, errmsg = loader_func(path)
+
+    if loader then
+      break
+    else
+      -- XXX error for when file isn't readable?
+      -- XXX error for when file isn't valid Lua (or loadable?)
+      tinsert(errors, sformat("no file '%q'", path))
+    end
+  end
+
+  if loader then
+    return loader
+  else
+    return tconcat(errors, '\n')
+  end
+end
+
+local function lua_loader(name)
+  return path_loader(name, package.path, loadfile)
+end
+
+local function get_init_function_name(name)
+  name = sgsub(name, '^.*%-', '', 1)
+  name = sgsub(name, '%.', '_')
+
+  return 'luaopen_' .. name
+end
+
+local function c_loader(name)
+  local init_func_name = get_init_function_name(name)
+
+  return path_loader(name, package.cpath, function(path)
+    return ploadlib(path, init_func_name)
+  end)
+end
+
+local function all_in_one_loader(name)
+  local init_func_name = get_init_function_name(name)
+  local base_name      = smatch(name, '^[^.]+')
+
+  return path_loader(base_name, package.cpath, function(path)
+    return ploadlib(path, init_func_name)
+  end)
+end
 
 local function require(name)
   if package.loaded[name] == nil then
@@ -36,15 +109,6 @@ local function require(name)
   return package.loaded[name]
 end
 
-_M.loaders = {}
-
-local loader_names = {
-  'preload_loader',
-  'lua_loader',
-  'c_loader',
-  'all_in_one_loader',
-}
-
 local loadermeta = {}
 
 function loadermeta:__call(...)
@@ -55,9 +119,12 @@ local function makeloader(loader_func, name)
   return setmetatable({ impl = loader_func, name = name }, loadermeta)
 end
 
-for i, loader in ipairs(package.loaders) do
-  _M.loaders[i] = makeloader(loader, loader_names[i])
-end
+_M.loaders = {
+  makeloader(preload_loader, 'preload'),
+  makeloader(lua_loader, 'lua'),
+  makeloader(c_loader, 'c'),
+  makeloader(all_in_one_loader, 'all_in_one'),
+}
 
 function meta:__call(name)
   return require(name)
